0c437d9eeac8233e09e7fb56a4f0af1b435f56ed
Justin Oberg <justin.oberg@icloud.com>
2015-09-05 11:21:31 -0700

    update email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+config.action_mailer.default_url_options = { :host => "http://www.ravnreviews.com" }
   config.action_mailer.delivery_method = :smtp 
   config.action_mailer.smtp_settings = {   
     address: 'smtp.gmail.com',   
