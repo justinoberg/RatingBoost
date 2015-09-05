32b140be4a9b35d84b86eab454f9e36d53a63a4b
Justin Oberg <justin.oberg@icloud.com>
2015-09-05 11:14:38 -0700

    added production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,16 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.smtp_settings = {   
+    address: 'smtp.gmail.com',   
+    port: 587,   
+    domain: 'gmail.com',   
+    user_name: 'ravnreviews@gmail.com',   
+    password:  'greeting',   
+    authentication: 'plain',   
+    enable_starttls_auto: true  
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -78,5 +88,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Required for devise remember to change localhost 3000 to actual application host
-  config.action_mailer.default_url_options = { host: 'ratingboost.com' }
+  config.action_mailer.default_url_options = { host: 'ravnreviwes.com' }
 end
